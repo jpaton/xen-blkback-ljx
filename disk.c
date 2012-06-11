8151c6f44f151a6b6913647cf794929bc1927a44
James Paton <paton@cs.wisc.edu>
2012-06-11 08:48:02 -0500

    deleted old unused files
    Signed-off-by: James Paton <paton@cs.wisc.edu>

--- disk.c
+++
@@ -1,5 +0,0 @@
-#include "disk.h"
-
-extern bool valid_boot_block(struct bio *bio, char *data) {
-	return false;
-}
--- disk.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DISK_H
-#define _DISK_H
-
-/**
- * Returns true of bio includes a valid boot block
- */
-extern bool valid_boot_block(struct bio *, char *);
-
-#endif
